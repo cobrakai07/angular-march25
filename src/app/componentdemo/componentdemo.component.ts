ffcb0d66503b7e82730a528afffcd95654f4fe53
Kai <dk786161616@gmail.com>
2025-03-18 10:17:30 +0530

    Component complete

--- src/app/componentdemo/componentdemo.component.ts
+++ src/app/componentdemo/componentdemo.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-componentdemo',// this will be used as tag in the parent component //u can change this tag name as you want
+  selector: 'app-componentdemo',// this will be used as tag in the parent component //u can change this tag name as you want// even u can name it as div tag, yes we can override HTML tags as well
   imports: [],
   templateUrl: './componentdemo.component.html',
   styleUrl: './componentdemo.component.css'
